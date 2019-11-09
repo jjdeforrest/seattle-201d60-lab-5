4f218d7555dbb802563cab18a07d7134ee877114
jjdeforrest <jjdeforrest@users.noreply.github.com>
2019-11-08 19:46:51 -0800

    problemTwo fixed

--- app.js
+++ app.js
@@ -17,26 +17,25 @@ function sum(a, b) { //eslint-disable-line
   var returnArray = [answer, Sent];
   return returnArray;
 }
-////asdfsf
+////asdfsffd
 testSum(4,7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
-/* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
-"The product of 5 and 9 is 45."
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
-
+//* Problem 2
+//Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function://
+//Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+//"The product of 5 and 9 is 45."
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    var answer= a*b;
+    var sent = 'The product of ' + a + ' and ' + b + ' is ' + answer + '.';
+    var returnArray = [answer, sent];
+    return returnArray;
 }
-
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ // uncomment it to run it
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
